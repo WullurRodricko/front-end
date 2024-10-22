ea8cb6ede0c448595b3fcebe548cf099f5f5764a
WullurRodricko <145328985+WullurRodricko@users.noreply.github.com>
2024-10-22 09:57:37 +0800

    Delete Pertemuan 14 directory

--- Pertemuan 14/utils.js
+++
@@ -1,9 +0,0 @@
-let fullName = "John Doe";
-
-let numbers = [1, 2, 3, 4, 5];
-
-const sayGreetings = () => `Hello World`;
-
-export { fullName, numbers as angka, sayGreetings };
-
-export default sayGreetings;
